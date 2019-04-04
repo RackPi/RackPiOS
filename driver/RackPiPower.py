d456e34de282eb3512daa28f12ab92faf5ac5c2b
Ediz Turcan <ediz.turcan@gmail.com>
2019-04-04 13:30:30 +0200

    comments added

--- driver/RackPiPower.py
+++ driver/RackPiPower.py
@@ -11,12 +11,17 @@
 from gpiozero import Button, DigitalInputDevice
 from subprocess import call
 
+# Connect button and LED for boot/shutdown activity on GPIO3
 button = Button(3)
+
+# Connect a short-circuit between GPIO 24 and 3V (e.g. pin 17+18)
 jumper = DigitalInputDevice(24)
 
 def shutdown():
     call("sudo shutdown -h now", shell=True)
 
+# Shutdown is only called when jumper is set (to be sure it's a rackpi)
+# and button is not pressed (because it's a switch)
 while True:
     if not button.is_pressed and jumper.value:
         shutdown()
