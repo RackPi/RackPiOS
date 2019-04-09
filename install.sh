06d2fe3c53d5304c781e4b46de608d2f8422b4c8
Ediz Turcan <ediz.turcan@gmail.com>
2019-04-09 14:48:16 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Copyright 2019 by RackPi
+# File:         install.sh
+# Description:  Installs all needed software and daemons for RackPi
+# Date:         2019-04-09
+# Version:      1.0
+# Author:       Ediz Turcan <ediz.turcan@icloud.com>
+# License:      MIT
+
+if [ "$(whoami)" != "root" ]; then
+  echo "Script must be run as root or with sudo"
+  exit -1
+fi
+
+apt-get update
+apt-get upgrade -y
+
+mkdir temp_rackpi
+cd temp_rackpi
+
+wget https://raw.githubusercontent.com/RackPi/RackPiOS/master/driver/RackPiPower.py
+cp RackPiPower.py /usr/local/bin/
+chmod +x /usr/local/bin/RackPiPower.py
+
+wget https://github.com/RackPi/PiLEDlights/releases/download/v1.0/PiLEDlights_v1.0.tar.gz
+tar xvfz PiLEDlights_v1.0.tar.gz
+cd PiLEDlights_v1.0
+cp * /usr/local/bin/
+chmod +x /usr/local/bin/actledPi
+chmod +x /usr/local/bin/hddledPi
+chmod +x /usr/local/bin/netledPi
+cd ..
+
+wget https://raw.githubusercontent.com/RackPi/RackPiOS/master/services/hddled.service
+wget https://raw.githubusercontent.com/RackPi/RackPiOS/master/services/netled.service
+wget https://raw.githubusercontent.com/RackPi/RackPiOS/master/services/rackpipower.service
+
+cp *.service /etc/systemd/system/
+systemctl enable hddled.service
+systemctl enable netled.service
+systemctl enable rackpipower.service
+
+cd ..
+rm -rf temp_rackpi
+reboot now
