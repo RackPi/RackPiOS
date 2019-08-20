32fc6219e83d76ab64345100b24191db7e4c5642
Ediz Turcan <0x6564697a@turcan.de>
2019-08-20 10:20:52 +0200

    project cleaned up

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 # Copyright 2019 by RackPi
 # File:         install.sh
 # Description:  Installs all needed software and daemons for RackPi
-# Date:         2019-04-09
-# Version:      1.0
-# Author:       Ediz Turcan <ediz.turcan@icloud.com>
+# Date:         2019-08-20
+# Version:      1.1
+# Author:       Ediz Turcan <ediz.turcan@gmail.com>
 # License:      MIT
 
 if [ "$(whoami)" != "root" ]; then
@@ -13,15 +13,12 @@ if [ "$(whoami)" != "root" ]; then
   exit -1
 fi
 
-apt-get update
-apt-get upgrade -y
+apt update
+apt upgrade -y
+apt install wiringpi -y
 
 mkdir temp_rackpi
 cd temp_rackpi
-
-wget https://raw.githubusercontent.com/RackPi/RackPiOS/master/driver/RackPiPower.py
-cp RackPiPower.py /usr/local/bin/
-chmod +x /usr/local/bin/RackPiPower.py
 
 wget https://github.com/RackPi/PiLEDlights/releases/download/v1.0/PiLEDlights_v1.0.tar.gz
 tar xvfz PiLEDlights_v1.0.tar.gz
@@ -34,12 +31,10 @@ cd ..
 
 wget https://raw.githubusercontent.com/RackPi/RackPiOS/master/services/hddled.service
 wget https://raw.githubusercontent.com/RackPi/RackPiOS/master/services/netled.service
-wget https://raw.githubusercontent.com/RackPi/RackPiOS/master/services/rackpipower.service
 
 cp *.service /etc/systemd/system/
 systemctl enable hddled.service
 systemctl enable netled.service
-systemctl enable rackpipower.service
 
 cd ..
 rm -rf temp_rackpi
